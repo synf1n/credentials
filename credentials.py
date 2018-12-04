c1de684db2cc5ae995e8253c34ee72b31c680594
synf1n <synf1n@guerrillamail.net>
2018-12-04 14:02:21 +1300

    Inital commit

---
+++ credentials.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+range_from = ''
+range_to = ''
+
+
+// get next credential
+// identify range to source from
+// lock database
+// read next credential
+// increment next credential
+// unlock database
